031252bd337ef226057059fb9301845464f23a77
kentonla <116829705+kentonla@users.noreply.github.com>
2025-12-18 14:00:32 -0800

    githubpages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/phonestadium',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
